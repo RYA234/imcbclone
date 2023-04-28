c8c6210ea6ca3699c78e8272f2057f6d614679bc
RYA234 <mryuabc@gmail.com>
2023-04-28 13:02:48 +0900

    Section1 5 Add a darkMode 3/○　React-icon実装　ダークモードに切り替えできるよう実装

--- src/components/DarkModeSwitch.jsx
+++ src/components/DarkModeSwitch.jsx
@@ -1,7 +1,17 @@
+"use client";
 import React from 'react'
+import {MdLightMode} from "react-icons/md";
+import {BsFillMoonFill} from "react-icons/bs"
+import {useTheme} from "next-themes";
+
 
 export default function DarkModeSwitch() {
-  return (
-    <div>DarkModeSwitch</div>
+    const{systemTheme,theme,setTheme} = useTheme();
+    const currentTheme = theme === "system" ? systemTheme : theme;
+    return (
+    <>  
+        { currentTheme === "dark" ? <MdLightMode onClick={() => setTheme("light")}/> :
+        <BsFillMoonFill onClick={() => setTheme("dark")}/>}
+    </>
   )
 }
