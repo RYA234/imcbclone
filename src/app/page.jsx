2ac6da1b058beb2f8529c847d705fd0c84cbbea0
RYA234 <mryuabc@gmail.com>
2023-04-28 20:27:28 +0900

    Section1 8 TMDBのAPIからデータ取得 取得結果をコンソール結果で確認

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,8 +1,25 @@
 import { Inter } from "@next/font/google"
 
-const inter = Inter({subsets:['latin']})
+const API_KEY = process.env.API_KEY;
 
-export default function Home() {
+
+export default async function Home({searchParams}) {
+  const genre = searchParams.genre || "fetchTrending";
+  
+  const res = await fetch(
+    `https://api.themoviedb.org/3/${
+      genre === "fetchTopRated" ? "movie/top_rated" : "trending/all/week"
+  }?api_key=${API_KEY}&language=en-Us%&page=1`,
+  {next:{revalidate: 10000}}
+  );
+
+  if(!res.ok){
+    throw new Error("Failed to fetch data");
+  }
+
+  const data = await res.json();
+  console.log(data);
+
   return (
     <h1 className='text-red-300'>HOME</h1>
   )
