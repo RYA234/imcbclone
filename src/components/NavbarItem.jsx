256846d8e03dcf3e243bf09c49e6d5b4f5b2be26
RYA234 <mryuabc@gmail.com>
2023-04-28 18:57:02 +0900

    Section1 7 navbar実装 2/2 hoverの処理追加　開いているNavの下線部をハイライト

--- src/components/NavbarItem.jsx
+++ src/components/NavbarItem.jsx
@@ -1,9 +1,19 @@
+"use client"
+
 import React from 'react'
 import Link from "next/link";
+import { useSearchParams } from "next/navigation";
 
 export default function NavbarItem({title,param}) {
-  return (
-    <div className={`font-semibold p-2`}>
+    const searchParams = useSearchParams();
+    const genre = searchParams.get("genre");
+
+    return (
+    <div className={`m-4 hover:text-amber-600 font-semibold p-2 ${
+        genre &&
+        genre === param &&
+        "underline underline-offeset-8 decoration-4 decoration-amber-500 rounded-lg"
+    }`}>
            <Link href={`/?genre=${param}`}>
                 {title}
            </Link>    
