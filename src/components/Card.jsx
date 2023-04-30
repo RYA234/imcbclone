427757d9d2badf9f78a6d69026ade775d21f4759
RYA234 <mryuabc@gmail.com>
2023-04-30 09:37:01 +0900

    Section1 11 CardComponent 2/3カードコンポーネントの画像を実装

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
 
 export default function Card({result}) {
   return (
     <div className="cursor-pointer sm:p-3 sm:hover:shadow-slate-400 sm:shadow-md rounded-lg sm:border sm:border-slate-400 sm:m-2 transition-shadow duration-200 group">
-        {result.id}
+      <Link href={`/movie/${result.id}`}>
+        <Image 
+         src={`https://image.tmdb.org/t/p/original/${
+            result.backdrop_path ||
+            result.poster_path
+         }`}
+         width={500}
+         height={300}
+         className="sm:rounded-t-lg
+         group-hover:opacity-80
+         transition-opacity
+         duration-200"
+
+         style={{
+              maxWidth:"100%",
+              height:"auto",
+         }}
+
+
+         placeholder="blur"
+         blurDataURL="/spinner.svg"
+         alt="image is not available"
+        ></Image>
+      </Link>
     </div>
   )
 }
