b028817d38453758713615aca46bf90ab87fbab9
RYA234 <mryuabc@gmail.com>
2023-04-30 10:21:40 +0900

    Section1 11 CardComponent 3/3 テキスト系をを実装　スクロールバーが出ないようにする。

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
+import { FiThumbsUp } from "react-icons/fi";
 
 export default function Card({result}) {
   return (
@@ -22,12 +23,18 @@ export default function Card({result}) {
               maxWidth:"100%",
               height:"auto",
          }}
-
-
          placeholder="blur"
          blurDataURL="/spinner.svg"
          alt="image is not available"
         ></Image>
+        <div className="p-2">
+            <p className="line-clamp-2 text-md">{result.overview}</p>
+            <h2 className="truncate text-lg font-bold">{result.title || result.name}</h2>
+            <p className="flex items-center ">
+                {result.release_date || result.first_air_date}
+                <FiThumbsUp className="h-5 mr-1 ml-3"/> {result.vote_count}
+            </p>
+        </div>
       </Link>
     </div>
   )
